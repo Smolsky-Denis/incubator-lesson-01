07383b189c0dddfd390285ede67a704f4a2e6c4e
Smolsky-Denis <smolskyden@gmail.com>
2026-04-12 13:17:46 +0200

    clear db

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,14 +2,5 @@ import {Idb} from "../videos/types/types";
 import {AvailableResolutions} from "../core/constants";
 
 export const db: Idb = {
-    videos: [{
-        id: 1,
-        title: 'Qwerty',
-        author: 'Qwerty Qwerty',
-        canBeDownloaded: true,
-        minAgeRestriction: 5,
-        createdAt: new Date(),
-        publicationDate: new Date(),
-        availableResolutions: AvailableResolutions.P144
-    }]
+    videos: []
 }
